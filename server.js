62db0ea28598cc04b1b1953563ee463460bc70cd
aquilestg <aquiles.toruno2015@gmail.com>
2017-05-29 20:09:09 -0600

    Preparando repositorio para heroku

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 var express = require('express');
 
 var app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(function (req, res, next) {
+    if (req.header['x-forwarded-proto'] === 'http') {
+        next();
+    } else {
+        res.redirect('http://' + req.hostname + req.url)
+    }
+})
 
 app.use(express.static('public'));
 
-app.listen(5000, function () {
-    console.log('Express server is running on port 5000');
+app.listen(PORT, function () {
+    console.log('Express server is running on port ' + PORT);
 });
